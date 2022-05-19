4b6b5614e3817c9f71f717defe7a1d1b90a85cfb
nishanth <nishukiran43@gmail.com>
2022-05-19 15:58:18 +0530

    commit

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text, useWindowDimensions } from "react-native";
 
-export default function App() {
+const App = () => {
+  const window = useWindowDimensions();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{`Window Dimensions: height - ${window.height}, width - ${window.width}`}</Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
+
+export default App;
